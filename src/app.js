0e19a0dfed94af89706ec1e594c6241f3c67ddda
Gonzalo Valentin Gomez <128619944+gonzalovalentingomez@users.noreply.github.com>
2024-08-07 13:39:07 -0300

    segunda-pre-entrega

--- src/app.js
+++ src/app.js
@@ -1,17 +1,57 @@
-const express = require("express"); 
-const app = express(); 
-const PUERTO = 8080;
-const productsRouter = require("./routes/products.router.js");
-const cartsRouter = require("./routes/carts.router.js"); 
+const express = require('express');
+const { create } = require('express-handlebars');
+const path = require('path');
+const http = require('http');
+const socketIo = require('socket.io');
 
-//Middleware: 
-app.use(express.json()); 
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
-//Rutas
-app.use("/api/products", productsRouter );
-app.use("/api/carts", cartsRouter); 
+const hbs = create({ extname: '.handlebars' });
 
+// Configuración de Handlebars
+app.engine('.handlebars', hbs.engine);
+app.set('view engine', '.handlebars');
+app.set('views', path.join(__dirname, 'views'));
 
-app.listen(PUERTO, () => {
-    console.log(`Escuchando en el http://localhost:${PUERTO}`); 
-})
+// Middleware para manejar datos en formato JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let products = [];
+
+app.get('/', (req, res) => {
+    res.render('home', { products });
+});
+
+app.get('/realtimeproducts', (req, res) => {
+    res.render('realTimeProducts', { products });
+});
+
+app.post('/products', (req, res) => {
+    const { product } = req.body;
+    products.push(product);
+    io.emit('productListUpdate', products);
+    res.status(201).send({ message: 'Producto agregado' });
+});
+
+app.delete('/products/:productName', (req, res) => {
+    const { productName } = req.params;
+    products = products.filter(product => product !== productName);
+    io.emit('productListUpdate', products);
+    res.status(200).send({ message: 'Producto eliminado' });
+});
+
+io.on('connection', (socket) => {
+    console.log('Nuevo cliente conectado');
+
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor en ejecución en el puerto ${PORT}`);
+});
